60a46e2b992d646b0689cbbbd69eddbfb8dcb34c
Nikita <n.chupakhin@yandex.ru>
2023-01-19 22:16:28 +0300

    Добавление работы с пользователем. Добавление авторизации с помощью Cookies.

--- webApp/Program.cs
+++ webApp/Program.cs
@@ -12,17 +12,93 @@ var builder = WebApplication.CreateBuilder(args);
 // Сервисы MVC
 builder.Services.AddControllersWithViews();
 
+// Подключение к БД
+string connection = "Host=localhost;Port=5432;Database=users;Username=postgres;Password=123456";
+builder.Services.AddDbContext<UsersContext>(options => options.UseNpgsql(connection));
+
+// Подключение аутентификации с помощью куки
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options => options.LoginPath = "/");
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-/*// Строка подключения к БД из файла конфигурации
-string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+app.UseAuthentication();   // Добавление middleware аутентификации 
+app.UseAuthorization();   // Добавление middleware авторизации 
 
-// Добавление контекста UsersContext в качестве сервиса в приложение
-builder.Services.AddDbContext<UsersContext>(options => options.UseNpgsql(connection));
+app.MapPost("/", async (string? returnUrl, HttpContext context, UsersContext db) =>
+{
+    // получаем из формы login и пароль
+    var form = context.Request.Form;
+    // если login и/или пароль не установлены, посылаем статусный код ошибки 400
+    if (!form.ContainsKey("login") || !form.ContainsKey("password"))
+        return Results.BadRequest("Логин и/или пароль не установлены");
 
-// получение данных
-// app.MapGet("/", (UsersContext db) => db.Users.ToList());*/
+    string login = form["login"];
+    string password = form["password"];
+
+    // находим пользователя 
+    User? user = db.Users.FirstOrDefault(u => u.Login == login && u.Password == password);
+
+    // если пользователь не найден, отправляем статусный код 401
+    if (user is null) return Results.Unauthorized();
+
+    var claims = new List<Claim> { 
+        new Claim(ClaimTypes.Name, user.Login),
+        new Claim(ClaimTypes.Role, user.Role)
+    };
+    // создаем объект ClaimsIdentity
+    ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+    // установка аутентификационных куки
+    await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+    return Results.Redirect(returnUrl ?? "/Authorization/AllUsers");
+});
+
+app.MapGet("/logout", async (HttpContext context) =>
+{
+    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    return Results.Redirect("/");
+});
+
+app.Map("/login", [Authorize] () => $"Hello World!");
+
+
+app.MapGet("/Authorization/AllUsers/api/users", async (UsersContext db) => await db.Users.ToListAsync());
+
+app.MapGet("/Authorization/AllUsers/api/users/{id:int}", async (int id, UsersContext db) =>
+{
+    User? user = await db.Users.FirstOrDefaultAsync(u => u.IdUser == id);
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+    return Results.Json(user);
+});
+
+app.MapDelete("/Authorization/AllUsers/api/users/{id:int}", async (int id, UsersContext db) =>
+{
+    User? user = await db.Users.FirstOrDefaultAsync(u => u.IdUser == id);
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+    db.Users.Remove(user);
+    await db.SaveChangesAsync();
+    return Results.Json(user);
+});
+
+app.MapPost("/Authorization/AllUsers/api/users", async (User user, UsersContext db) =>
+{
+    // добавляем пользователя в массив
+    await db.Users.AddAsync(user);
+    await db.SaveChangesAsync();
+    return user;
+});
+
+app.MapPut("/Authorization/AllUsers/api/users", async (User userData, UsersContext db) =>
+{
+    var user = await db.Users.FirstOrDefaultAsync(u => u.IdUser == userData.IdUser);
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+    user.Login = userData.Login;
+    user.Password = userData.Password;
+    user.Role = userData.Role;
+    await db.SaveChangesAsync();
+    return Results.Json(user);
+});
 
 
 // Configure the HTTP request pipeline.
@@ -40,6 +116,6 @@ app.UseRouting();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Authorization}/{action=AllUsers}/{id?}");
+    pattern: "{controller=Authorization}/{action=Authorization}/{id?}");
 
 app.Run();
