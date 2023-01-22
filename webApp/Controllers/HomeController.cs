12f542c0a8fc965baab51545cf8f7656d3895639
Nikita <n.chupakhin@yandex.ru>
2023-01-22 15:22:48 +0300

    Создание контроллера Account - для авторизации и аутентификации. Создание контроллера Home - для разделения представлений для каждой роли. Перенос функционала авторизации с map методов в методы контроллера.

---
+++ webApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webApp.Models;
+
+namespace webApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize(Policy = "OnlyForAdmin")]
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "OnlyForTeacher")]
+        public IActionResult Teacher()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "OnlyForStudent")]
+        public IActionResult Student()
+        {
+            return View();
+        }
+    }
+}
